365a1e0607f26a4860ceb07dadb3690587744c84
ldebski <debskilukasz9@gmail.com>
2020-07-22 17:46:43 +0200

    Labeler

---
+++ labeler/Assets/HiResScreenShots.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Diagnostics;
+
+public class HiResScreenShots : MonoBehaviour
+{
+    public int resWidth = 400;
+    public int resHeight = 400;
+
+    private bool takeHiResShot = false;
+
+    public static string ScreenShotName(int width, int height)
+    {
+        return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png",
+                             Application.dataPath,
+                             width, height,
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    public void TakeHiResShot()
+    {
+        takeHiResShot = true;
+    }
+
+    static void lineChanger(string newText, string fileName, int line_to_edit)
+    {
+        // lineChanger("new content for this line" , "sample.text" , 34);
+        string[] arrLine = File.ReadAllLines(fileName);
+        arrLine[line_to_edit - 1] = newText;
+        File.WriteAllLines(fileName, arrLine);
+    }
+
+    static string getZnacznikPosition(string matName)
+    {
+        GameObject znacznik = GameObject.Find("Znacznik");
+        return string.Format("{0},{1},{2},{3}",
+                            matName,
+                            znacznik.transform.position,
+                            znacznik.transform.eulerAngles,
+                            znacznik.transform.localScale
+                            );
+    }
+
+    string matName;
+    public void SaveZnacznikInfo()
+    {
+        string dataFilePath = string.Format("{0}/screenshots/data/data.txt", Application.dataPath);
+        matName = GameObject.Find("Brzuch").GetComponent<Renderer>().material.name;
+        int ind = matName.IndexOf(" (");
+        matName = matName.Substring(0, ind);
+        string line;
+        int counter = 1;
+        bool found = false;
+        string lineToWrite = getZnacznikPosition(matName); // TODO FORMAT idMat,reszta
+        using (System.IO.StreamReader file =
+           new System.IO.StreamReader(dataFilePath))
+        {
+            while ((line = file.ReadLine()) != null)
+            {
+                var list = line.Split(',');//.ToList();
+                if (list[0] == matName)
+                {
+                    found = true;
+                    break;
+                }
+                counter++;
+            }
+        }
+        if (found)
+        {
+            UnityEngine.Debug.Log(counter);
+            lineChanger(lineToWrite, dataFilePath, counter);
+        }
+        else
+        {
+            File.AppendAllText(dataFilePath, lineToWrite + Environment.NewLine);
+        }
+    }
+
+    void LateUpdate()
+    {
+        takeHiResShot |= Input.GetKeyDown("k");
+        if (takeHiResShot)
+        {
+            UnityEngine.Debug.Log(Directory.GetCurrentDirectory());
+            RenderTexture rt = new RenderTexture(resWidth, resHeight, 24);
+            GetComponent<Camera>().targetTexture = rt;
+            Texture2D screenShot = new Texture2D(resWidth, resHeight, TextureFormat.RGB24, false);
+            GetComponent<Camera>().Render();
+            RenderTexture.active = rt;
+            screenShot.ReadPixels(new Rect(0, 0, resWidth, resHeight), 0, 0);
+            GetComponent<Camera>().targetTexture = null;
+            RenderTexture.active = null; // JC: added to avoid errors
+            Destroy(rt);
+            byte[] bytes = screenShot.EncodeToPNG();
+            string filename = ScreenShotName(resWidth, resHeight);
+            System.IO.File.WriteAllBytes(filename, bytes);
+            SaveZnacznikInfo();
+            UnityEngine.Debug.Log(string.Format("Took screenshot to: {0}", filename));
+            takeHiResShot = false;
+        }
+    }
+}
---
+++ labeler/Assets/MoveZnacznik.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class MoveZnacznik : MonoBehaviour
+{
+    Vector3 mousePos;
+    bool mouseDown = false;
+    bool mouseRotateDown = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown()
+    {
+        mouseDown = true;
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    bool MouseMove()
+    {
+        return (Input.GetAxis("Mouse X") != 0) || (Input.GetAxis("Mouse Y") != 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float val = 0.005f;
+        // position
+        if (Input.GetKey(KeyCode.UpArrow) && transform.position.y < 1.0f)
+        {
+            transform.position += Vector3.up * val;
+        }
+        if (Input.GetKey(KeyCode.DownArrow) && transform.position.y > -1.0f)
+        {
+            transform.position -= Vector3.up * val;
+        }
+        if (Input.GetKey(KeyCode.RightArrow) && transform.position.x < 1.0f)
+        {
+            transform.position += Vector3.right * val;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) && transform.position.x > -1.0f)
+        {
+            transform.position -= Vector3.right * val;
+        }
+        // rotations
+        if (Input.GetKey("w") && (transform.eulerAngles.x > 300.0f || transform.eulerAngles.x < 70.0f))
+        {
+            transform.eulerAngles -= Vector3.right * val * 100;
+        }
+        if (Input.GetKey("s") && (transform.eulerAngles.x > 290.0f || transform.eulerAngles.x < 60.0f))
+        {
+            transform.eulerAngles += Vector3.right * val * 100;
+        }
+        if (Input.GetKey("a") && (transform.eulerAngles.y > 300.0f || transform.eulerAngles.y < 70.0f))
+        {
+            transform.eulerAngles -= Vector3.up * val*100;
+        }
+        if (Input.GetKey("d") && (transform.eulerAngles.y > 290.0f || transform.eulerAngles.y < 60.0f))
+        {
+            transform.eulerAngles += Vector3.up * val*100;
+        }
+        // mouse wheel for scale
+        if (Input.mouseScrollDelta.y == 1 && transform.localScale.x < 1.0f)
+        {
+            transform.localScale += (Vector3.up * val * 5 + Vector3.right * val * 5);
+        }
+        if (Input.mouseScrollDelta.y == -1 && transform.localScale.x > 0.3f)
+        {
+            transform.localScale -= (Vector3.up * val * 5 + Vector3.right * val * 5);
+        }
+
+        // move with mouse
+        if (Input.GetMouseButtonUp(0))
+        {
+            mouseDown = false;
+        }
+        if (mouseDown && MouseMove())
+        {
+            Vector3 currMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position += (currMousePos - mousePos);
+            mousePos = currMousePos;
+        }
+
+        // rotate with mouse
+        if (Input.GetMouseButtonDown(1))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseRotateDown = true;
+        }
+        if (Input.GetMouseButtonUp(1))
+        {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mouseRotateDown = false;
+        }
+        if (mouseRotateDown && MouseMove())
+        {
+            Vector3 currMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 rotateVec = (currMousePos - mousePos);
+            Vector3 rotateVecFixed = new Vector3(rotateVec.y, rotateVec.x, 0);
+            transform.eulerAngles += rotateVecFixed * 70;
+            if (transform.eulerAngles.x > 60.0f && transform.eulerAngles.x < 280)
+            {
+                transform.eulerAngles = new Vector3(60.0f, transform.eulerAngles.y, 0);
+            }
+            if (transform.eulerAngles.x < 300.0f && transform.eulerAngles.x > 80)
+            {
+                transform.eulerAngles = new Vector3(300.0f, transform.eulerAngles.y, 0);
+            }
+            if (transform.eulerAngles.y > 60.0f && transform.eulerAngles.y < 280)
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, 60.0f, 0);
+            }
+            if (transform.eulerAngles.y < 300.0f && transform.eulerAngles.y > 80)
+            {
+                transform.eulerAngles = new Vector3(transform.eulerAngles.x, 300.0f, 0);
+            }
+            mousePos = currMousePos;
+        }
+
+    }
+}
